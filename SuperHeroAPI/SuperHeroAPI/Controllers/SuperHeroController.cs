2bc525e130ba98fa1588d5256d5de64c17ac521f
McDony Lee <mcdonyleee@gmail.com>
2022-10-24 15:50:39 +1100

    Basic table showing from DB

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        //constructor
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet] //this is an attributes from c# https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/attributes/ 
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            /* return new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Name = "Spider Man From c#",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            }; */
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+    }
+}
